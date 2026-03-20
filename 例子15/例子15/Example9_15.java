e1d8293cec2809a35fa784dc35bbae94b32c2e8b
ACKADE <138556213+ACKADE@users.noreply.github.com>
2026-03-20 15:01:17 +0800

    Delete 例子15/例子15 directory

--- 例子15/例子15/Example9_15.java
+++
@@ -1,9 +0,0 @@
-public class Example9_15 {
-   public static void main(String args[]){
-      WindowTriangleView win=new WindowTriangleView();
-      win.setTitle("???MVC??"); 
-      win.setBounds(100,100,720,260);
-   }
-}
-
-
--- 例子15/例子15/SetFont.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JComponent;
-import java.awt.Font;
-public class SetFont {
-     public static void setFont(Font f,JComponent ...component){
-         for(JComponent c:component)
-            c.setFont(f);
-     }
-}
